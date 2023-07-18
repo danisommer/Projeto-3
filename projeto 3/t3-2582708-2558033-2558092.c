ebec52a3c30b7b6371e51257e9e57c692d66e261
danisommer <75692301+danisommer@users.noreply.github.com>
2023-07-18 17:34:46 -0300

    FINAL

--- projeto 3/t3-2582708-2558033-2558092.c
+++ projeto 3/t3-2582708-2558033-2558092.c
@@ -1,87 +1,71 @@
-/*============================================================================*/
-/* CSF13 - 2023-1 - TRABALHO 3                                                */
-/*----------------------------------------------------------------------------*/
-/* Daniel Zaki Sommer - 2582708                                               */
-/* Henrique dos Santos Pagliarini  - 2558033                                  */
-/* Matheus Dias - 2558092                                                     */
-/*============================================================================*/
-/** Arquivo de entrega para o 3o trabalho da disciplina CSF13 (Fundamentos de
- * Programação 1), 2023-1, Universidade Tecnológica Federal do Paraná. */
-/*============================================================================*/
-
 #include "trabalho3.h"
 #include <math.h>
-
-/*============================================================================*/
 
 #define NUM_FAIXAS 8
 #define TOLERANCIA 35
 #define ALTURA_MINIMA 3
-
 #define COMPRIMENTO_MINIMO 45
 #define COMPRIMENTO_MOTO 72
 #define COMPRIMENTO_MEDIO 122
 #define COMPRIMENTO_LONGO 302
-
-/*============================================================================*/
-/** Classifica um veículo de acordo com seu comprimento.
- *
- * Parâmetros: int comprimento: o comprimento do veículo a ser analisado.
- *             int contagem[N_TIPOS_DE_VEICULOS]: vetor para contagem dos tipos de veículos.
- *
- * Valor de Retorno: NENHUM (os dados são escritos no vetor) */
+#define SATURACAO_MINIMA 50
 
 void classificaVeiculo(int comprimento, int contagem[N_TIPOS_DE_VEICULOS]) {
   if (comprimento <= COMPRIMENTO_MOTO)
-    contagem[0]++;  // Incrementa a contagem de motos.
+    contagem[0]++;
   else if (comprimento <= COMPRIMENTO_MEDIO)
-    contagem[1]++;  // Incrementa a contagem de veículos médios.
+    contagem[1]++;
   else if (comprimento <= COMPRIMENTO_LONGO)
-    contagem[2]++;  // Incrementa a contagem de veículos longos.
+    contagem[2]++;
   else
-    contagem[3]++;  // Incrementa a contagem de veículos muito longos.
+    contagem[3]++;
 }
 
-/*----------------------------------------------------------------------------*/
-/** Remove o fundo da imagem comparando-a com uma imagem de fundo.
- *
- * Parâmetros: Imagem3C* img: ponteiro para a imagem a ser modificada.
- *             Imagem3C* bg: ponteiro para a imagem de fundo a ser comparada.
- *
- * Valor de Retorno: NENHUM (os dados são modificados diretamente na imagem de entrada) */
+float calcularSaturacao(int r, int g, int b) {
+  float max = fmax(r, fmax(g, b));
+  float min = fmin(r, fmin(g, b));
+
+  if (max == 0)
+    return 0;
+
+  float delta = max - min;
+  float saturation = delta / max;
+
+  return saturation;
+}
 
 void removeFundo(Imagem3C* img, Imagem3C* bg) {
   int i, j;
 
   for (i = 0; i < bg->altura; i++) {
     for (j = 0; j < bg->largura; j++) {
-      // Compara a diferença absoluta dos canais de cor de cada pixel com a tolerância definida.
-      if (abs(bg->dados[0][i][j] - img->dados[0][i][j]) <= TOLERANCIA &&
-          abs(bg->dados[1][i][j] - img->dados[1][i][j]) <= TOLERANCIA &&
-          abs(bg->dados[2][i][j] - img->dados[2][i][j]) <= TOLERANCIA) {
-            // Define os canais de cor do pixel como zero para removê-lo.
-            img->dados[0][i][j] = 0;
-            img->dados[1][i][j] = 0;
-            img->dados[2][i][j] = 0;
+      int r_img = img->dados[0][i][j];
+      int g_img = img->dados[1][i][j];
+      int b_img = img->dados[2][i][j];
+
+      int r_bg = bg->dados[0][i][j];
+      int g_bg = bg->dados[1][i][j];
+      int b_bg = bg->dados[2][i][j];
+
+      float saturation = calcularSaturacao(r_img, g_img, b_img);
+
+      if (abs(r_img - r_bg) <= TOLERANCIA &&
+          abs(g_img - g_bg) <= TOLERANCIA &&
+          abs(b_img - b_bg) <= TOLERANCIA &&
+          saturation >= SATURACAO_MINIMA) {
+        img->dados[0][i][j] = 0;
+        img->dados[1][i][j] = 0;
+        img->dados[2][i][j] = 0;
       }
     }
   }
 }
 
-/*----------------------------------------------------------------------------*/
-/** Verifica os veículos presentes em cada faixa da imagem.
- *
- * Parâmetros: Imagem3C* img: ponteiro para a imagem a ser analisada.
- *             int contagem[N_TIPOS_DE_VEICULOS]: vetor para contagem dos tipos de veículos.
- *             int* total_veiculos: ponteiro para a variável que armazenará o total de veículos.
- *
- * Valor de Retorno: NENHUM (os dados são escritos nos vetores e variável de saída) */
-
 void verificaVeiculosFaixa(Imagem3C* img, int contagem[N_TIPOS_DE_VEICULOS], int* total_veiculos) {
   int comprimento = 0, contagem_pixels;
   int i, j, k, faixa, max_faixa;
 
-  faixa = img->altura / NUM_FAIXAS;  // Calcula a altura de cada faixa.
+  faixa = img->altura / NUM_FAIXAS;
   max_faixa = faixa;
 
   for (i = 0; max_faixa < img->altura; i += faixa, max_faixa += faixa) {
@@ -90,31 +74,21 @@ void verificaVeiculosFaixa(Imagem3C* img, int contagem[N_TIPOS_DE_VEICULOS], int
 
       for (k = i; k < max_faixa && contagem_pixels < ALTURA_MINIMA; k++) {
         if (img->dados[0][k][j] || img->dados[1][k][j] || img->dados[2][k][j])
-          contagem_pixels++;  // Conta a quantidade de pixels diferentes de preto na faixa.
+          contagem_pixels++;
       }
 
       if (contagem_pixels == ALTURA_MINIMA)
-        comprimento++;  // Incrementa o comprimento do veículo.
+        comprimento++;
       else {
         if (comprimento > COMPRIMENTO_MINIMO) {
-          *total_veiculos += 1;  // Incrementa o total de veículos.
+          *total_veiculos += 1;
           classificaVeiculo(comprimento, contagem);
         }
-        comprimento = 0;  // Reinicia o comprimento para o próximo veículo.
+        comprimento = 0;
       }
-
     }
   }
 }
-
-/*----------------------------------------------------------------------------*/
-/** Conta e classifica os veículos presentes na imagem.
- *
- * Parâmetros: Imagem3C* img: ponteiro para a imagem a ser analisada.
- *             Imagem3C* bg: ponteiro para a imagem de fundo a ser comparada.
- *             int contagem[N_TIPOS_DE_VEICULOS]: vetor para contagem dos tipos de veículos.
- *
- * Valor de Retorno: o número total de veículos encontrados. */
 
 int contaVeiculos(Imagem3C* img, Imagem3C* bg, int contagem[N_TIPOS_DE_VEICULOS]) {
   int total_veiculos = 0, i;
@@ -127,5 +101,3 @@ int contaVeiculos(Imagem3C* img, Imagem3C* bg, int contagem[N_TIPOS_DE_VEICULOS]
 
   return total_veiculos;
 }
-
-/*============================================================================*/
